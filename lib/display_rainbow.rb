3b3a1de3b5db3e36be7217c442bd529a30f80748
Maura Slavin <mauramslavin@gmail.com>
2018-08-31 19:47:48 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,12 +1,7 @@
 def display_rainbow(colors)
   inits = ['R', 'O', 'Y', 'G', 'B', 'I','V']
-  for i in 0..6 do
-    print inits[i] + ': ' + color[i]
-    if i != 6
-      print ','
-    end
-    i +=1
-  end
+  puts "#{inits[0]}: #{colors[0]}, #{inits[1]}: #{colors[1]}, #{inits[2]}: #{colors[2]}, #{inits[3]}: #{colors[3]}, #{inits[4]}: #{colors[4]}, #{inits[5]}: #{colors[5]}, #{inits[6]}: #{colors[6]}"
+ end
 
 display_rainbow(["red", "orange", "yellow", "green", "blue", "indigo", "violet"])
 
